6ee6df2a8a30b105850cec41d15fc7ea8e659b61
Annal3sa <114109863+Annal3sa@users.noreply.github.com>
2023-11-15 08:21:58 -0600

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    
+
+
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - enemyRb.transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
